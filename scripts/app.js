4b142f96d5d09da717558b7d24670d488eb3ec29
MasAbbasi <masoud.abbasi.71@gmail.com>
2022-07-16 21:08:16 +0430

    Set Hamburger Menu Icon Animation

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+const hamburgerMenuBtn = document.querySelector(".hamburger-menu-icon")
+
+hamburgerMenuBtn.addEventListener ("click",function(){
+	hamburgerMenuBtn.classList.toggle("hamburger-menu-icon--open")
+})
