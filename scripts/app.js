d87740656095f9abf1c60a1aca8e4b05e3627735
MasAbbasi <masoud.abbasi.71@gmail.com>
2022-07-17 12:41:10 +0430

    Set Mobile Menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
 const hamburgerMenuBtn = document.querySelector(".hamburger-menu-icon")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 hamburgerMenuBtn.addEventListener ("click",function(){
 	hamburgerMenuBtn.classList.toggle("hamburger-menu-icon--open")
+	mobileMenu.classList.toggle("mobile-menu--open")
 })
